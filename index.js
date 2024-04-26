e017a7f73049381288b656634ed01fdcc7eb8545
Joachim Susatiyo <joe.susatiyo@gmail.com>
2024-04-26 15:31:24 +0700

    navbar things

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
-var imageindex = 425;
-
-let slideIndex = 0;
-showSlides();
-
-function plusSlides(x) {
-  var image = document.getElementById("carousel-img");
-  imageindex = imageindex + x;
-
-  // Fade out current image
-  image.style.opacity = 0;
-
-  // Change image source after fade out
-  setTimeout(function () {
-    image.src =
-      "Photos Live In (2 GB)/Photos Live In/GOPR0" + imageindex + ".JPG";
-  }, 500); // Adjust timing to match the transition duration
-
-  // Fade in new image
-  setTimeout(function () {
-    image.style.opacity = 1;
-  }, 600); // Adjust timing to match the transition duration
+function test (x)
+{
+	console.log(x)
+	
 }
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
 
-  // Hide all slides
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
 
-  // Move to the next slide
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
 
-  // Display the current slide
-  slides[slideIndex - 1].style.display = "block";
 
-  // Change slide every 3 seconds (3000 milliseconds)
-  setTimeout(showSlides, 3000);
+
+var imageindex = Math. floor(Math. random()*10) + 1
+var maxindex = 10
+var autoslide = setInterval(plusSlides, 6000);
+
+function plusSlides () {
+    var image = document.getElementById("carousel-img");
+    imageindex = imageindex + 1
+    if (imageindex > maxindex)
+    {
+        imageindex = 1
+    }
+
+
+    // Fade out current image
+    image.style.opacity = 0;
+
+    // Change image source after fade out
+    setTimeout(function() {
+        image.src = "Photos_Simplified/" + imageindex + ".jpg"
+    }, 500); // Adjust timing to match the transition duration
+
+    // Fade in new image
+    setTimeout(function() {
+        image.style.opacity = 1;
+    }, 600); // Adjust timing to match the transition duration
 }
+
+function adjust ()
+{
+    var elements = document.getElementsByClassName("navigation");
+    var img = document.getElementById("carousel-img");
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
+
+    if (windowHeight > windowWidth) {
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.fontSize = "3vw";
+        }
+        document.getElementById("title").style.fontSize = "3vh"
+        document.getElementById("title2").style.fontSize = "8vh"
+        img.style.width = "auto"
+        img.style.height = "100%"
+        
+    } else {
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.fontSize = "3vh";
+        }
+        document.getElementById("title").style.fontSize = "3vw"
+        document.getElementById("title2").style.fontSize = "8vw"
+        img.style.width = "100%"
+        img.style.height = "auto"
+    }
+
+    
+
+}
+
+plusSlides()
+adjust()
